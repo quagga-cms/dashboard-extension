dbea1ffe0f476fbae0b086b90288f1683e4f74bf
Puleeno Nguyen <puleeno@gmail.com>
2023-08-20 20:21:19 +0700

    Create dashboard controller

--- routes.php
+++ routes.php
@@ -2,10 +2,8 @@
 
 declare(strict_types=1);
 
-use App\Application\Middleware\SessionMiddleware;
-use App\Application\Settings\SettingsInterface;
+use App\Core\Settings\SettingsInterface;
 use App\Http\Middleware\Authenticate;
-use DI\Container;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
---
+++ src/Http/Controllers/DashboardController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace PuleenoCMS\Dashboard\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class DashboardController extends Controller
+{
+}
