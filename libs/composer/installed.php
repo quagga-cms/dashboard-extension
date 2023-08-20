21f3a8d7b41425c28c4fa728cf9382cea9b47951
Puleeno Nguyen <puleeno@gmail.com>
2023-08-20 16:50:49 +0700

    Create structure for dashboard extension

---
+++ libs/composer/installed.php
@@ -0,0 +1,25 @@
+<?php
+
+return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '4e4c255a595ce8b4df3e91f6381fe4e86a0a5a87',
+        'name' => 'puleeno-cms/dashboard',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'puleeno-cms/dashboard' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '4e4c255a595ce8b4df3e91f6381fe4e86a0a5a87',
+            'dev_requirement' => false,
+        ),
+    ),
+);
---
+++ src/DashboardExtension.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace PuleenoCMS\Dashboard;
+
+use App\Core\Extension;
+
+class DashboardExtension extends Extension
+{
+    protected $isBuiltIn = true;
+}
